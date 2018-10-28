764a707084f80f4a0a8bb1a3863346ad8d315852
Johann Chopin <johannchopin@pm.me>
2018-10-28 16:26:57 +0100

    Add the comments

--- Uebungen/ueb01-j/src/Artikel.java
+++ Uebungen/ueb01-j/src/Artikel.java
@@ -2,9 +2,10 @@
  * 
  * Die Klasse simuliert ein Artikel
  * 
- * @author leparesseux
+ * @author Guidoux Alexandre && Chopin Johann
  * @version 1.0
  */
+
 public class Artikel {
 	private int nummer;
 	private String bezeichnung;
@@ -12,16 +13,21 @@ public class Artikel {
 	
 	/**
 	 * IllegalArgumentException mit @msg zurückgesendet, wenn die @bedigung falsch ist
-	 * @param bedigung
-	 * @param msg die Nachricht der Ausnahme
+	 * @param bedigung  Die Bedigung
+	 * @param msg       Die Nachricht der Ausnahme
 	 */
 	public void check(boolean bedigung, String nachricht) {
 		if (!bedigung)
 			throw new IllegalArgumentException(nachricht);
 	}
 	
+	/**
+	 * @param nummer      Der Nummer muss 4-stellig sein
+	 * @param bezeichnung Der Bezeichnung muss nicht leer sein
+	 * @param bestand     Der Bestand darf nie kleiner als 0 werden
+	 */
 	Artikel(int nummer, String bezeichnung, int bestand) {
-		check(bezeichnung != null || !bezeichnung.isEmpty(), "Der Bezeichnung muss nicht leer sein.");
+		check(bezeichnung != null || !bezeichnung.isEmpty(), "Der Bezeichnung muss nicht leer sein");
 		check(999 < nummer && nummer < 10000, "Der Nummer muss 4-stellig sein");
 		
 		this.nummer = nummer;
@@ -29,24 +35,31 @@ public class Artikel {
 		setBestand(bestand);
 	}
 	
+	/**
+	 * @param nummer      Der Nummer muss 4-stellig sein
+	 * @param bezeichnung Der Bezeichnung muss nicht leer sein
+	 */
 	Artikel(int nummer, String bezeichnung){
 		this(nummer, bezeichnung, 0);
 	}
 	
+	
+	
+	
 	public int getNummer() {
 		return nummer;
 	}
-
 	public String getBezeichnung() {
 		return bezeichnung;
 	}
-
 	public int getBestand() {
 		return bestand;
 	}
 
+	
+	
+	
 	/**
-	 *  
 	 * @param bestand muss immer >= 0 sein
 	 */
 	public void setBestand(int bestand) {
@@ -54,38 +67,27 @@ public class Artikel {
 		this.bestand = bestand;
 	}
 	
-	public String toString() {
-		return "Artikel : " + getNummer() +
-			   " Bezeichnung: " + getBezeichnung() + 
-			   " Bestand: " + getBestand();
-	}
-	
+
+	/**
+	 * @param zusatz muss immer > 0 sein
+	 */
 	public void zugang(int zusatz) {
 		check(zusatz > 0, "Der Zusatz darf nicht <= 0 sein.");
 		setBestand(this.bestand + zusatz);
 	}
 
 	/**
-	 *  
-	 * @param absatz muss immer <= this.bestand sein
+	 * @param absatz muss immer > 0 sein
 	 */
 	public void abgang(int absatz) {
-		check(absatz > 0, "Der Zusatz darf nicht <= 0 sein.");
+		check(absatz > 0, "Der Abgang darf nicht <= 0 sein.");
 		setBestand(this.bestand - absatz);
 	}
 	
-	/**
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println("OK ! :)");
-		
-		Artikel Artikel1 = new Artikel(4242, "staubsauger", 10);
-		System.out.println(Artikel1);
-		Artikel1.abgang(10);
-		System.out.println(Artikel1);
-		Artikel1.zugang(10);
-		System.out.println(Artikel1);
-	}	
+	
+	public String toString() {
+		return "Artikel : " + getNummer() +
+			   " Bezeichnung: " + getBezeichnung() + 
+			   " Bestand: " + getBestand();
+	}
 }
