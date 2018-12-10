31ebb566792893652e62831f0b28837c685025a0
lyokolux <lyokolux@protonmail.com>
2018-12-10 16:24:53 +0100

    :art: and :white_check_mark: for stringCount

--- Uebungen/ueb06/a/stringCountTest.java
+++ Uebungen/ueb06/a/stringCountTest.java
@@ -11,31 +11,23 @@ import org.junit.Test;
  */
 public class stringCountTest
 {
-    private static final int[] NULL = {0, 0};
+    private static final int NULL = 0;
     
     // BAD VALUES
     @Test
     public void bad_value1(){
        String[] tab = {"b0njour", "B0NJOUR", "hell0", "HELL0",
                        "t4g", "T4G", "42", "l33t", "L33T"};
-       assertArrayEquals("Strings mit Nummer",
-                         Tools.stringCount(tab),
-                         NULL);
-    }
-    
-    @Test
-    public void bad_value2(){
-       String[] tab = {"JoJo", "bonjouR", "assertArrayEquals", "Ok"};
-       assertArrayEquals("Strings mit Groß-und Kleinbuchstaben",
-                         Tools.stringCount(tab),
-                         NULL);
+       assertEquals("Strings mit Nummer",
+                    Tools.stringCount(tab),
+                    NULL);
     }
     
     @Test
     public void bad_value3(){
        String[] tab = {"guten tag", "hello world", "l apero du captain", 
                        " ", " gluecksbaerchen", "gluecksbaerchen "};
-       assertArrayEquals("Strings mit Leerzeichnen",
+       assertEquals("Strings mit Leerzeichnen",
                          Tools.stringCount(tab),
                          NULL);
     }
@@ -44,27 +36,41 @@ public class stringCountTest
     public void bad_value4(){
        String[] tab = {"%s/java/python/g", "ein+ein", "--'", "<script>",
                        ":D", "python>java", "€uro", ":(){ :|: & };:"};
-       assertArrayEquals("Strings mit Sonderzeichen",
-                         Tools.stringCount(tab),
-                         NULL);  
+       assertEquals("Strings mit Sonderzeichen",
+                    Tools.stringCount(tab),
+                    NULL);  
     }
     
     // GOOD VALUES
     @Test
     public void good_value1(){
        String[] tab = {"JAVA", "PYTHON", "BASH", "C", "LUA", "HASKELL"};
-       int[] result = {tab.length, 0};
-       assertArrayEquals("Strings mit Großbuchstaben",
-                         Tools.stringCount(tab),
-                         result);  
+       assertEquals("Strings mit Großbuchstaben",
+                    Tools.stringCount(tab),
+                    tab.length);
     }
     
     @Test
     public void good_value2(){
-       String[] tab = {"elixir", "clojure", "rust", "go"};
-       int[] result = {0, tab.length};
-       assertArrayEquals("Strings mit Kleinbuchstaben",
-                         Tools.stringCount(tab),
-                         result);  
+       String[] tab = {"elixir", "clojure", "rust", "go", "d"};
+       assertEquals("Strings mit Kleinbuchstaben",
+                    Tools.stringCount(tab),
+                    tab.length);  
+    }
+        
+    @Test
+    public void good_value3(){
+       String[] tab = {"JoJo", "bonjouR", "assertArrayEquals", "Ok"};
+       assertEquals("Strings mit Groß-und Kleinbuchstaben",
+                    Tools.stringCount(tab),
+                    tab.length);
+    }
+    
+    @Test
+    public void good_value4(){
+       String[] tab = {"straßenbahn", "étoile", "à", "pétale", "königin"};
+       assertEquals("Strings mit Sonderbuchstaben",
+                    Tools.stringCount(tab),
+                    tab.length);
     }
 }
