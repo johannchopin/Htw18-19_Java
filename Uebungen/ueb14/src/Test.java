801bb2a2b21e1c61292dc2b5ba8255177f92ebf6
Johann Chopin <johannchopin@pm.me>
2019-04-16 00:19:51 +0200

    Auto stash before merge of "master" and "origin/master"

--- Uebungen/ueb14/src/Test.java
+++ Uebungen/ueb14/src/Test.java
@@ -9,6 +9,7 @@
  */
 public class Test
 {    
+
     /**
      * Main-Methode : Erzeuget des LagerDialog-Objekts und start der
      * Hauptschleife.
@@ -17,6 +18,7 @@ public class Test
         Mitarbeiter m1 = new Mitarbeiter("Max", "Mustermann", "max@htwsaar.de");
         Mitarbeiter m2 = new Mitarbeiter("John", "Doe", "john@htwsaar.de");
 
+        Raum r = new Raum(18, 0, 1);
         Raum r1 = new Raum(18,0,1);
         Raum r2 = new Raum(2,1,9);
         Raum r3 = new Raum(2,1,10);
@@ -25,6 +27,12 @@ public class Test
         m1.reserviere(r2, new Uhrzeit(14, 30), new Uhrzeit(16, 30), "WebTech");
         m2.reserviere(r2, new Uhrzeit(12, 30), new Uhrzeit(13, 30), "Prog II");
         m2.reserviere(r3, new Uhrzeit(10, 30), new Uhrzeit(11, 30), "ITM");
+
+        System.out.println(m1);
+        System.out.println("---");
+        System.out.println(r);
+        System.out.println("---");
+        System.out.println(r1);
     }
 
 }
