9050d888879e7e83f98217a293c80955b611d1c0
Johann Chopin <johannchopin@pm.me>
2019-05-24 16:29:03 +0100

    Add aufgabe1

--- Uebungen/ueb18/Aufgabe1.java
+++ Uebungen/ueb18/Aufgabe1.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 /**
  * Décrivez votre classe Aufgabe1 ici.
@@ -8,7 +9,45 @@
 public class Aufgabe1
 {
     
-    public double iterate(double x0, int n, fn) {
+    public void iterate(double x0, int n, FunctionInterface fn) {
+        double end = x0 + n;
+        
+        for(double i = x0; i<=end; i++) {
+            double result = fn.apply(i);
+            
+            System.out.println(i + " -> " + result);
+        }
     }
     
+    public FunctionInterface iLambda() {
+        return (double n) -> 2 * n;
+    }
+    
+    public FunctionInterface iiLambda() {
+        return (double n) -> 0.5 * n;
+    }
+    
+    public FunctionInterface iiiLambda() {
+        Random rand = new Random();
+        float a = rand.nextFloat();
+        return (double n) -> a * n * (n - 1);
+    }
+    
+    protected void run() {
+        Aufgabe1 test = new Aufgabe1();
+        
+        System.out.println("\niLambda (2x) :");
+        test.iterate(2.0, 10, test.iLambda());
+        
+        System.out.println("\niiLambda (0.5x) :");
+        test.iterate(2.0, 10, test.iiLambda());
+        
+        System.out.println("\niiiLambda (ax(x − 1)) :");
+        test.iterate(2.0, 10, test.iiiLambda());
+    }
+    
+    public static void main(String[] args) {
+       Aufgabe1 test = new Aufgabe1();
+       test.run();
+    }
 }
---
+++ Uebungen/ueb18/FunctionInterface.java
@@ -0,0 +1,12 @@
+
+/**
+ * FunctionInterface
+ *
+ * @author  Guidoux Alexandre && Chopin Johann
+ * @version 1.0.0
+ */
+
+public interface FunctionInterface
+{
+    public double apply(double n);
+}
