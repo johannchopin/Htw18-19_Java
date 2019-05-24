6992cbbe76ed7bccfdab0d22033f7283f6e69c8c
Johann Chopin <johannchopin@pm.me>
2019-05-24 14:40:53 +0100

    Init ueb18

---
+++ Uebungen/ueb18/Aufgabe1.java
@@ -0,0 +1,14 @@
+
+/**
+ * Décrivez votre classe Aufgabe1 ici.
+ *
+ * @author Guidoux Alexandre & Chopin Johann
+ * @version 1.0.0
+ */
+public class Aufgabe1
+{
+    
+    public double iterate(double x0, int n, fn) {
+    }
+    
+}
---
+++ Uebungen/ueb18/Aufgabe1Interface.java
@@ -0,0 +1,18 @@
+
+/**
+ * Décrivez votre interface Aufgabe1Interface ici.
+ *
+ * @author  (votre nom)
+ * @version (un numéro de version ou une date)
+ */
+
+public interface Aufgabe1Interface
+{
+    /**
+     * Exemple d'entête de méthode - remplacez ce commentaire par le vôtre
+     *
+     * @param  y    le paramètre de cette méthode
+     * @return        le résultat retourné par exempleDeMethode
+     */
+    int exempleDeMethode(int y);
+}
