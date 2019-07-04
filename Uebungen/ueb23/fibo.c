80edebdf0921e71e403fcc16977fb97b7306503e
Lyokolux <lyokolux@protonmail.com>
2019-07-04 16:04:12 +0200

    2. Aufgabe fertig

--- Uebungen/ueb23/fibo.c
+++ Uebungen/ueb23/fibo.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
+int fibo(int n){
+    if(n <= 1)
+        return n;
+    int a = 0, b = 1, c;
+    int aux;
+    while(n-- >= 2){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
+
+int main(int argc, char *argv[]){
+    char *nptr;
+    int n = strtol(argv[1], &nptr, 10);
+    if(n < 0){
+        printf("Arg must be an integer and > 0.\n");
+        return -1;
+    }
+    printf("%d\n", fibo(n));
     return 0;
 }
