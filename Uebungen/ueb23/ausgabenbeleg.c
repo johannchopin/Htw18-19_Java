871a049e33f581c8d1a3c32ad9ca6628fcb7be34
Lyokolux <lyokolux@protonmail.com>
2019-07-04 16:30:28 +0200

    1. Aufgabe fertig

--- Uebungen/ueb23/ausgabenbeleg.c
+++ Uebungen/ueb23/ausgabenbeleg.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void){
+void ausgabeBeleg(double nettopreis){
+    double mwst = nettopreis / 20;
+    double bruttopreis = nettopreis + mwst;
+    double skonto = bruttopreis * 0.02;
+    double rechnungbetrag = bruttopreis * 0.98;
+
+    printf("Nettopreis               Euros %6.2f\n", nettopreis);
+    printf("+ 20%% MwSt               Euros %6.2f\n", mwst);
+    printf("=====================================\n");
+    printf("Bruttopreis              Euros %6.2f\n", bruttopreis);
+    printf("- 2%% Skonto              Euros %6.2f\n", skonto);
+    printf("=====================================\n");
+    printf("Rechnungbetrag           Euros %6.2f\n", rechnungbetrag);
+}
+
+int main(int argc, char* argv[]){
+    char *nptr;
+    double n = strtod(argv[1], &nptr);
+    if(n < 0){
+        printf("Arg must be an integer and > 0.\n");
+        return -1;
+    }
+    ausgabeBeleg(n);
     return 0;
 }
