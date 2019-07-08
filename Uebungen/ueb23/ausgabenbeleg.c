c82bec313fb9de6ee630b2936aec8ccd5e915c3d
johannchopin <johannchopin@pm.me>
2019-07-08 17:57:27 +0200

    Add printBillLine() anf fix 'make test' bug

--- Uebungen/ueb23/ausgabenbeleg.c
+++ Uebungen/ueb23/ausgabenbeleg.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+#define BILL_LINE_SEPARATOR "================================================================="
+
+
+char * printBillLine(char * lineTitle, float value, int withLineSeparator)
+{
+    printf("%-15s %10s EURO %6.2f\n", lineTitle, "", value);
+
+   if(withLineSeparator) {
+       printf("%.*s\n", 38, BILL_LINE_SEPARATOR);
+   }
+}
+
+
 void ausgabeBeleg(double nettopreis){
-    double mwst = nettopreis / 20;
+    double mwst = nettopreis * 0.2;
     double bruttopreis = nettopreis + mwst;
     double skonto = bruttopreis * 0.02;
     double rechnungbetrag = bruttopreis * 0.98;
 
-    printf("Nettopreis               Euros %6.2f\n", nettopreis);
-    printf("+ 20%% MwSt               Euros %6.2f\n", mwst);
-    printf("=====================================\n");
-    printf("Bruttopreis              Euros %6.2f\n", bruttopreis);
-    printf("- 2%% Skonto              Euros %6.2f\n", skonto);
-    printf("=====================================\n");
-    printf("Rechnungbetrag           Euros %6.2f\n", rechnungbetrag);
+    printBillLine("Nettopreis", nettopreis, 0);
+    printBillLine("+ 20% MwSt", mwst, 1);
+    printBillLine("Bruttopreis", bruttopreis, 0);
+    printBillLine("- 2% Skonto", skonto, 1);
+    printBillLine("Rechnungsbetrag", rechnungbetrag, 0);
 }
 
 double argvToLong(int argc, char** argv){
@@ -30,6 +42,6 @@ double argvToLong(int argc, char** argv){
 }
  
 int main(int argc, char* argv[]){
-    ausgabeBeleg(argvToLong(argc, argv));
+    ausgabeBeleg(atof(argv[1]));
     return 0;
 }
