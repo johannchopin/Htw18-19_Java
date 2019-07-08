675d09034dc09387745204a0b28a5290e15ea684
johannchopin <johannchopin@pm.me>
2019-07-08 18:17:42 +0200

    Update fibo comment

--- Uebungen/ueb23/fibo.c
+++ Uebungen/ueb23/fibo.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /*
- * Return the nth number of the fibonacci's suit
+ * Return the n first numbers of the fibonacci's suit
  */
 int fibo(int n){
     int first = 0, second = 1, next, c;
@@ -29,10 +29,12 @@ int fibo(int n){
 int main(int argc, char *argv[]){
     char *nptr;
     int n = strtol(argv[1], &nptr, 10);
+
     if(n < 0){
         printf("Arg must be an integer and > 0.\n");
         return -1;
     }
+
     fibo(n);
 
     return 0;
